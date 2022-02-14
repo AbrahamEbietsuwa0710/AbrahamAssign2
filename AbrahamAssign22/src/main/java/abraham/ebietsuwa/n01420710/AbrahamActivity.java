566ef6c51e455179d71f7ee515f6997b80d9f0f1
AbrahamEbietsuwa0710 <kighimiebietsuwa@gmail.com>
2022-02-13 19:18:30 -0500

    Update the content of .gitignore

---
+++ AbrahamAssign22/src/main/java/abraham/ebietsuwa/n01420710/AbrahamActivity.java
@@ -0,0 +1,17 @@
+/*Abraham Ebietsuwa, n01420710, RNB
+* */
+
+package abraham.ebietsuwa.n01420710;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AbrahamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
